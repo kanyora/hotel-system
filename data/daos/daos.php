5d127e669a56fa503e70c10f9aa56717abb9ff14
Ian Onesmus Mukewa <e-nesmukewa@hotmail.com>
2012-02-01 11:34:35 +0300

    changes to file...

--- data/daos/daos.php
+++ data/daos/daos.php
@@ -1,9 +1,3 @@
 <?php
-	require_once("AuthDAO.php");
-	
-	require_once("staff/DoctorDAO.php");
-	require_once("staff/NurseDAO.php");
-	require_once("staff/PharmacistDAO.php");
-	require_once("staff/StockPersonelDAO.php");
-	
+	require_once("AuthDAO.php");	
 ?>
